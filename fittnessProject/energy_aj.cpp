6c81d5fd6759aa012244e937cf1bb8dc1e6646b5
Andrew Davis <aramishu@gmail.com>
2012-03-10 19:38:20 +0000

    added unit superclass and energy subclass

---
+++ fittnessProject/energy_aj.cpp
@@ -0,0 +1,5 @@
+#include "energy_aj.h"
+
+energy_aj::energy_aj()
+{
+}
---
+++ fittnessProject/energy_aj.h
@@ -0,0 +1,12 @@
+#ifndef ENERGY_AJ_H
+#define ENERGY_AJ_H
+
+#include "unit_aj.h"
+
+class energy_aj : public unit_aj
+{
+public:
+    energy_aj();
+};
+
+#endif // ENERGY_AJ_H
